32885d0179ed5e96f8103439447f4e0cd9c211bb
Sharvzz <loganathansharvesh14@gmail.com>
2025-08-28 12:05:38 +0530

    login-bg-change

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,17 +1,17 @@
 import {Link} from "react-router-dom";
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-[100vh] text-center bg-[url('/bg-home.jpg')] bg-cover ">
-      <h1 className="text-4xl font-bold mb-4 text-white">Welcome to Credora</h1>
-      <p className="text-lg max-w-xl text-white">
+    <div className="flex flex-col items-center justify-center min-h-[100vh] text-center bg-[url('/bg-home1.jpg')] bg-cover ">
+      <h1 className="text-4xl font-bold mb-4 text-black">Welcome to Credora</h1>
+      <p className="text-lg max-w-xl text-black">
         Credora helps you manage your personal finances with ease. Track your income, expenses, view insightful reports, and take control of your financial future.
       </p>
       <br />
       <br />
 
       <div className="flex flex-row justify-between space-x-5">
-        <Link to="/login"><button className="bg-black text-white px-4 py-2 rounded-xl hover:bg-white hover:text-black hover:border hover:black">Login</button> </Link>
-        <Link to="/signup"><button className="bg-black text-white px-4 py-2 rounded-xl hover:bg-white hover:text-black hover:border hover:black">Sign Up</button></Link>
+        <Link to="/login"><button className="bg-black text-white px-4 py-2 rounded-xl">Login</button> </Link>
+        <Link to="/signup"><button className="bg-black text-white px-4 py-2 rounded-xl">Sign Up</button></Link>
       </div>
     </div>
   );
