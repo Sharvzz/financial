ef6f5e6bc77078ee2a56575c1637d1442201faa4
Sharvzz <loganathansharvesh14@gmail.com>
2025-09-18 15:50:05 +0530

    stable update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,26 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
   server: {
-<<<<<<< HEAD
     proxy: {
-      "/api": {
-        target: "http://localhost:5000",
+      '/api': {
+        target: 'http://localhost:5000',
         changeOrigin: true,
         secure: false,
       },
     },
   },
-=======
-    host: '0.0.0.0',
-    port: 5000,
-    strictPort: true,
-    hmr: {
-      clientPort: 5000
-    }
-  }
->>>>>>> 3f6bd940676980f6a40c8ec8d5d66e7dd5aec6b0
 })
